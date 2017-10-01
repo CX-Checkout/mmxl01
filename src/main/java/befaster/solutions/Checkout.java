b05a46727fb23af8af905a6c3e907c29665b1a86
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-01 21:16:56 +0300

    Sun Oct 01 21:16:56 EEST 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -2,10 +2,29 @@ package befaster.solutions;
 
 public class Checkout {
     public static Integer checkout(String skus) {
-        Integer[] counts = new Integer[4];
+        int aItems = 0;
+        int bItems = 0;
+        int cItems = 0;
+        int dItems = 0;
         for (char c : skus.toCharArray()) {
-
+            switch (c) {
+                case 'A':
+                    aItems++;
+                    break;
+                case 'B':
+                    bItems++;
+                    break;
+                case 'C':
+                    cItems++;
+                    break;
+                case 'D':
+                    dItems++;
+                    break;
+                default:
+                    return -1;
+            }
         }
 
+        
     }
 }
